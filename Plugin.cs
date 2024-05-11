a66692d2f9266e28f749def2ec1736467ef96fa2
Megga__ <165313235+Megg4underscore@users.noreply.github.com>
2024-05-11 20:17:07 +0100

    Original Upload

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AllBindsForCheats
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("ULTRAKILL.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private Harmony patch;
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            patch = new Harmony(PluginInfo.PLUGIN_GUID);
+            patch.PatchAll(typeof(Plugin));
+            patch.PatchAll(typeof(BindPatch));
+        }
+    }
+
+    public static class BindPatch
+    {
+
+        [HarmonyPatch(typeof(CheatBinds), "Awake"), HarmonyPrefix]
+        static void AllBindsAllowed(ref string ___bannedBinds)
+        {
+            Debug.Log("ALL BINDS FOR CHEATS HAS BEEN APPLIED TO YOUR ULTRAKILL");
+            ___bannedBinds = "";
+        }
+    }
+}
---
+++ obj/Debug/netstandard2.0/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace AllBindsForCheats
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "AllBindsForCheats";
+        public const string PLUGIN_NAME = "AllBindsForCheats";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
